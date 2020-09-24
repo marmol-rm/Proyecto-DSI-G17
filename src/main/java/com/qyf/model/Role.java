3190c6c8051be3d3317eea83cd4000bd6def5e72
Marcelo <mr14015@ues.edu.sv>
2020-09-23 21:18:13 -0600

    Segundo

---
+++ src/main/java/com/qyf/model/Role.java
@@ -0,0 +1,47 @@
+package com.qyf.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="role")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id_rol;
+	private String nombre;
+	private String descripcion;
+	
+	@Override
+	public String toString() {
+		return "Role [id_role=" + id_rol + ", nombre=" + nombre + ", descripcion=" + descripcion + "]";
+	}
+
+	public Integer getId_role() {
+		return id_rol;
+	}
+
+	public void setId_role(Integer id_role) {
+		this.id_rol = id_role;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+}
+
---
+++ src/main/java/com/qyf/model/User.java
@@ -0,0 +1,8 @@
+package com.qyf.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class User {
+	private Integer id_user;
+}
