4c15be9a6295671577066a2ec8c33c6be6f6ded2
Marcelo <mr14015@ues.edu.sv>
2020-09-28 21:02:34 -0600

    Haciendo validaciones

--- src/main/java/com/qyf/controller/DepartamentoController.java
+++ src/main/java/com/qyf/controller/DepartamentoController.java
@@ -6,16 +6,9 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import com.qyf.interfaceService.IDepartamentoServ;
 import com.qyf.model.Departamento;
-
-import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
 @Controller
 @RequestMapping
--- src/main/resources/static/funciones.js
+++ src/main/resources/static/funciones.js
@@ -1,5 +1,14 @@
-function eliminarDepartamento(id){
-	swal("Desea eliminar este Departamento?", {
+function check(input) {
+   if (input.value == 0) {
+     input.setCustomValidity('Debe ser diferente a 0');
+   } else {
+     // input is fine -- reset the error message
+     input.setCustomValidity('');
+   }
+ }
+ 
+function mensajeEliminar(id){
+	swal("Esta seguro?", {
 	 buttons: ["No", "Si"],
 	});
 	.then((Si)=> {
