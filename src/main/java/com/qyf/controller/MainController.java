866ffe2b2ce2a98ca613c953f468c4522a180272
Marcelo <mr14015@ues.edu.sv>
2020-10-09 16:43:45 -0600

    Intento 9

--- src/main/java/com/qyf/controller/MainController.java
+++ src/main/java/com/qyf/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.qyf.interfaceService.IUsuarioServ;
+import com.qyf.model.Role;
 import com.qyf.model.Usuario;
 
 @Controller
@@ -18,6 +19,14 @@ public class MainController {
 	public String inicio() {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		Usuario user = new Usuario();
+		Role rol =new Role();
+		rol.setId_rol(1);
+		rol.setNombre("Administrador");
+		rol.setDescripcion("Rol admin");
+		user.setNombres("prueba");
+		user.setApellidos("prueba");
+		user.setActivo(1);
+		user.setRole(rol);
 		user.setEmail("prueba@email.com");
 		String encodedPass = encoder.encode("123");
 		user.setPassword(encodedPass); //Se guarda el password codificado
