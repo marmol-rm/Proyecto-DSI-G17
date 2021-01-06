bae44a44d2c88779403aa3ebe0ea3c92720d94de
Marcelo <34018240+marmol-rm@users.noreply.github.com>
2021-01-06 02:12:25 -0600

    Otro intento

--- src/main/java/com/qyf/ProyectoqyfApplication.java
+++ src/main/java/com/qyf/ProyectoqyfApplication.java
@@ -3,16 +3,11 @@ package com.qyf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.qyf.interfaceService.IMainService;
-
 @SpringBootApplication
 public class ProyectoqyfApplication {
-	@Autowired
-	private IMainService main;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoqyfApplication.class, args);
-		main.inicializar();
 	}
 
 }
--- src/main/java/com/qyf/controller/MainController.java
+++ src/main/java/com/qyf/controller/MainController.java
@@ -2,20 +2,20 @@ package com.qyf.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.qyf.interfaceService.IMainService;
 
 @Controller
 public class MainController {
-/*@Autowired
+	@Autowired
 	private IMainService main;
 	
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String inicio() {
 		//Inicializa valores fijos en BD
 		main.inicializar();
 		
 		return "index";
-	}*/
+	}
 }
